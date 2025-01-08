91ba393950fc47d455e5f5af24f30f11efb50928
NISHANT <69218488+NishantCoder108@users.noreply.github.com>
2025-01-08 07:39:48 +0000

    Airdrop implemented

---
+++ airdrop.ts
@@ -0,0 +1,28 @@
+import wallet from "./dev-wallet.json";
+import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+
+const connection = new Connection("https://api.devnet.solana.com");
+
+// Airdrop Tokens:
+(async () => {
+    try {
+        const airdropSignature = await connection.requestAirdrop(
+            keypair.publicKey,
+            2 * LAMPORTS_PER_SOL
+        );
+
+        console.log(
+            `Success! Check out TX here: https://explorer.solana.com/tx/${airdropSignature}?cluster=devnet`
+        );
+    } catch (error) {
+        console.log(`Oops! Something went wrong: ${error}`);
+    }
+})();
+
+// Getting Balances:
+(async () => {
+    const balance = await connection.getBalance(keypair.publicKey);
+    console.log("Balance: ", balance);
+})();
